00444e726405e2cd0f34fe49ad1776ccf8089c50
WilisArum02 <wilissarrum@gmail.com>
2025-12-16 03:09:29 +0700

    Update

--- src/data/HomeSection.jsx
+++ src/data/HomeSection.jsx
@@ -1,6 +1,35 @@
+// import HomeImage from "../asset/good-eye.png";
+// export const homeSection = {
+//   Image: HomeImage,
+//   content:
+//     '<h2>Sehatkan Matamu, Mulai dari Sekarang</h2><p>Gunakan Eye Care App untuk deteksi dini kesehatan mata. Cek sekarang dan lindungi penglihatanmu!</p><p><a href="/CekMata" className="tbl-pink">Cek Mata Sekarang</a></p>',
+// };
+
+import { useNavigate } from "react-router-dom";
 import HomeImage from "../asset/good-eye.png";
-export const homeSection = {
-  Image: HomeImage,
-  content:
-    '<h2>Sehatkan Matamu, Mulai dari Sekarang</h2><p>Gunakan Eye Care App untuk deteksi dini kesehatan mata. Cek sekarang dan lindungi penglihatanmu!</p><p><a href="/CekMata" className="tbl-pink">Cek Mata Sekarang</a></p>',
-};
+import { cekMataSection } from "../data/CekMataSection";
+
+function HomeSection() {
+  const navigate = useNavigate();
+
+  return (
+    <section>
+      <img src={HomeImage} alt="Eye Care" />
+
+      <h2>Sehatkan Matamu, Mulai dari Sekarang</h2>
+      <p>
+        Gunakan Eye Care App untuk deteksi dini kesehatan mata.
+        Cek sekarang dan lindungi penglihatanmu!
+      </p>
+
+      <button
+        className="tbl-pink"
+        onClick={() => navigate("../data/CekMataSection")}
+      >
+        Cek Mata Sekarang
+      </button>
+    </section>
+  );
+}
+
+export default HomeSection;
