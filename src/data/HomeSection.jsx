8bddce91c283e6c469a8dccc6ad32b7407c94d39
WilisArum02 <wilissarrum@gmail.com>
2025-12-16 03:29:59 +0700

    Update

--- src/data/HomeSection.jsx
+++ src/data/HomeSection.jsx
@@ -5,30 +5,17 @@
 //     '<h2>Sehatkan Matamu, Mulai dari Sekarang</h2><p>Gunakan Eye Care App untuk deteksi dini kesehatan mata. Cek sekarang dan lindungi penglihatanmu!</p><p><a href="/CekMata" className="tbl-pink">Cek Mata Sekarang</a></p>',
 // };
 
-import { useNavigate } from "react-router-dom";
-import HomeImage from "../asset/good-eye.png";
+import { Link } from "react-router-dom";
 
-function HomeSection() {
-  const navigate = useNavigate();
-
-  return (
-    <section>
-      <img src={HomeImage} alt="Eye Care" />
-
+export const homeSection = {
+  Image: HomeImage,
+  content: (
+    <>
       <h2>Sehatkan Matamu, Mulai dari Sekarang</h2>
-      <p>
-        Gunakan Eye Care App untuk deteksi dini kesehatan mata.
-        Cek sekarang dan lindungi penglihatanmu!
-      </p>
-
-      <button
-        className="tbl-pink"
-        onClick={() => navigate("/CekMata") }
-      >
+      <p>Gunakan Eye Care App untuk deteksi dini kesehatan mata.</p>
+      <Link to="/CekMata" className="tbl-pink">
         Cek Mata Sekarang
-      </button>
-    </section>
-  );
-}
-
-export default HomeSection;
+      </Link>
+    </>
+  ),
+};
