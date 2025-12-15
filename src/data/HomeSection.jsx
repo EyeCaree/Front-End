e329323371f2b717a019deff29a4131eb4fd12bb
WilisArum02 <wilissarrum@gmail.com>
2025-12-16 04:01:51 +0700

    Update

--- src/data/HomeSection.jsx
+++ src/data/HomeSection.jsx
@@ -1,21 +1,24 @@
-import HomeImage from "../asset/good-eye.png";
+// import HomeImage from "../asset/good-eye.png";
 // export const homeSection = {
 //   Image: HomeImage,
 //   content:
 //     '<h2>Sehatkan Matamu, Mulai dari Sekarang</h2><p>Gunakan Eye Care App untuk deteksi dini kesehatan mata. Cek sekarang dan lindungi penglihatanmu!</p><p><a href="/CekMata" className="tbl-pink">Cek Mata Sekarang</a></p>',
 // };
 
-import { Link } from "react-router-dom";
+import HomeImage from "../asset/good-eye.png";
 
 export const homeSection = {
   Image: HomeImage,
-  content: (
-    <>
-      <h2>Sehatkan Matamu, Mulai dari Sekarang</h2>
-      <p>Gunakan Eye Care App untuk deteksi dini kesehatan mata.</p>
-      <Link to="/CekMata" className="tbl-pink">
+  content: `
+    <h2>Sehatkan Matamu, Mulai dari Sekarang</h2>
+    <p>Gunakan Eye Care App untuk deteksi dini kesehatan mata.</p>
+    <p>
+      <a href="/CekMata" class="tbl-pink">
         Cek Mata Sekarang
-      </Link>
-    </>
-  ),
+      </a>
+    </p>
+  `,
 };
+
+
+
